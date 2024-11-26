8227fb6442995b4ad1ec4d36e24aa7e987385ce5
Gulilil <juan.csantoso@gmail.com>
2024-11-26 16:38:20 +0700

    add progress integration no 1

--- src/edge_detection/laplacian_edge_detection.m
+++ src/edge_detection/laplacian_edge_detection.m
@@ -1,20 +1,22 @@
-function image_edge_detection = laplacian_edge_detection(img, nKernel)
+function image_edge_detection = laplacian_edge_detection(img, nKernel, kernel_str)
     % Konversi Image ke Grayscale
     if size(img, 3) == 3
         img = rgb2gray(img);
     end
 
     % Check apakah ukuran kernel ganjil dan bilangan bulat
-    while mod(nKernel, 2) == 0 || nKernel ~= floor(nKernel)
+    if mod(nKernel, 2) == 0 || nKernel ~= floor(nKernel)
         fprintf("Ukuran kernel harus ganjil. Silahkan masukkan ukuran kernel yang benar.\n");
-        nKernel = input("Masukkan ukuran kernel n x n (n ganjil): ");
+        % nKernel = input("Masukkan ukuran kernel n x n (n ganjil): ");
+    else
+        % Membuat Laplacian Kernel
+        laplacian_kernel = generate_laplacian_kernel(double(nKernel), kernel_str);
+            
+        % Deteksi tepi menggunakan metode Laplacian memanfaatkan konvolusi
+        image_edge_detection = conv2(img, laplacian_kernel, 'same');
+    
+        % Konversi gambar hasil deteksi tepi ke tipe data uint8
+        image_edge_detection = uint8(image_edge_detection);
+
     end
 
-    % Membuat Laplacian Kernel
-    laplacian_kernel = generate_laplacian_kernel(double(nKernel));
-        
-    % Deteksi tepi menggunakan metode Laplacian memanfaatkan konvolusi
-    image_edge_detection = conv2(img, laplacian_kernel, 'same');
-
-    % Konversi gambar hasil deteksi tepi ke tipe data uint8
-    image_edge_detection = uint8(image_edge_detection);
--- src/matrix/generate_laplacian_kernel.m
+++ src/matrix/generate_laplacian_kernel.m
@@ -1,18 +1,18 @@
-function laplacian_kernel = generate_laplacian_kernel(nKernel)
+function laplacian_kernel = generate_laplacian_kernel(nKernel, laplacian_kernel_str)
     % Check apakah kernel valid
     validKernel = false;
     
     while ~validKernel
         % Minta input dari pengguna untuk kernel Laplacian
-        disp(['Masukkan matriks Laplacian kernel berukuran ', num2str(nKernel), 'x', num2str(nKernel)]);
+        % disp(['Masukkan matriks Laplacian kernel berukuran ', num2str(nKernel), 'x', num2str(nKernel)]);
 
-        % Informasi terkait matriks kernel Laplacian
-        fprintf('Syarat Matriks Laplacian:\n')
-        fprintf('1. Matriks kernel harus simetris\n');
-        fprintf('2. Jumlah elemen dalam kernel harus nol\n');
+        % % Informasi terkait matriks kernel Laplacian
+        % fprintf('Syarat Matriks Laplacian:\n')
+        % fprintf('1. Matriks kernel harus simetris\n');
+        % fprintf('2. Jumlah elemen dalam kernel harus nol\n');
         
-        % Input kernel dalam bentuk string
-        laplacian_kernel_str = input('Masukkan matriks kernel: ', 's');
+        % % Input kernel dalam bentuk string
+        % laplacian_kernel_str = input('Masukkan matriks kernel: ', 's');
         
         % Konversi input string ke dalam bentuk numerik
         laplacian_kernel = str2num(laplacian_kernel_str);
